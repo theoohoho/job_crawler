bcfa004505dc2f1e70b27f643ec16b07ad5eb4df
theoohoho <topslimer@gmail.com>
2021-01-20 20:15:29 +0800

    Moved parsed_data model to scheam folder

---
+++ schema/parsed_data.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from typing import Optional
+class ParsedData(BaseModel):
+    """ Paesed data format"""
+    job_title: str
+    company: str
+    job_area: Optional[str]
+    job_desc: Optional[str]
+    job_url: str
+    company_url: str
+    update_time: Optional[str]
