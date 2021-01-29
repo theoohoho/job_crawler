d2cca2fea34ef43a78c6f5da98b91846a88b6920
theoohoho <topslimer@gmail.com>
2021-01-29 17:02:21 +0800

    Added core config settings

---
+++ core/config.py
@@ -0,0 +1,28 @@
+from pydantic import BaseSettings, PostgresDsn, validator
+from typing import Optional, Dict, Any
+
+
+class ConfigSettings(BaseSettings):
+    # database configuration
+    POSTGRES_USERNAME: Optional[str] = None
+    POSTGRES_PASSWORD: Optional[str] = None
+    POSTGRES_HOST: Optional[str] = None
+    POSTGRES_PORT: Optional[str] = None
+    POSTGRES_DB: Optional[str] = None
+    POSTGRES_URI: Optional[PostgresDsn] = None
+
+    @validator("POSTGRES_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USERNAME"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_HOST"),
+            port=values.get("POSTGRES_PORT"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+
+config_settings = ConfigSettings()
